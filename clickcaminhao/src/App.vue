f061682adf488f8c29cdc74f14c352d9c4919f78
Frank-Willians <franksocial10@hotmail.com>
2021-12-18 08:28:04 -0400

    Primeiro commit de ajustes

---
+++ clickcaminhao/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<h1>
+  Olá Mato Grosso do Sul
+</h1>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
